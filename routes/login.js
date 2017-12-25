bf04c0f4f3753beb6704a1e8d8442488ab3c9817
chengqun <chengqun@meiqia.com>
2017-12-25 17:22:48 +0800

    添加mysql操作,防止注入

--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,41 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.render('login', { name: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('login', {name: 'Express'});
+});
+
+/**
+ * 用户登录验证
+ */
+router.post('/a', function (req, res, next) {
+//获得post参数 obj{ name: 'admin', pwd: 'admin' }
+    var json = req.body;
+    var name = json.name;
+    var pwd = json.pwd;
+//操作数据库
+    req.getConnection(function (err, conn) {
+        if (err) {
+            return next(err);
+        } else {
+//nodejs sql防止注入  http://www.dengzhr.com/node-js/877
+            conn.query('SELECT name FROM user WHERE name = ? AND password = ?', [name,pwd], function (err, result) {
+                if (err) {
+                    return next(err);
+                } else {
+                    var str = JSON.stringify(result);
+                    var jso = JSON.parse(str);
+                    //jso [ { name: 'admin',password:'admin'} ]
+                    //响应ajax
+                    if (jso[0]) {
+                        res.json({success: true});
+                    } else {
+                        res.json({success: false});
+                    }
+                }
+            });
+        }
+    });
 });
 module.exports = router;
 
